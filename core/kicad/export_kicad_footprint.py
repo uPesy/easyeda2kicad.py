74a864ce4fc5c35884f6c341e4d821736d725060
Alexis <alexis@upesy.com>
2022-01-02 16:47:19 +0100

    First release

---
+++ core/kicad/export_kicad_footprint.py
@@ -0,0 +1,390 @@
+# Global import
+from itertools import chain
+from math import isnan, cos, sin, pi, sqrt, acos
+
+# Local import
+from core.kicad.parameters_kicad import *
+from core.easyeda.parameters_easyeda import ee_footprint
+
+# ---------------------------------------
+
+def to_radians(n: float):
+    return (n/180.0)*pi
+
+
+def to_degrees(n: float):
+    return (n / pi) * 180.0
+
+# Elliptical arc implementation based on the SVG specification notes
+
+
+def compute_arc(
+    x0: float,
+    y0: float,
+    rx: float,
+    ry: float,
+    angle: float,
+    largeArcFlag: bool,
+    sweepFlag: bool,
+    x: float,
+    y: float
+):
+
+    # Compute the half distance between the current and the final point
+    dx2 = (x0 - x) / 2.0
+    dy2 = (y0 - y) / 2.0
+
+    # Convert angle from degrees to radians
+    angle = to_radians(angle % 360.0)
+    cosAngle = cos(angle)
+    sinAngle = sin(angle)
+
+    # Step 1 : Compute (x1, y1)
+    x1 = cosAngle * dx2 + sinAngle * dy2
+    y1 = -sinAngle * dx2 + cosAngle * dy2
+
+    # Ensure radii are large enough
+    rx = abs(rx)
+    ry = abs(ry)
+    Prx = rx * rx
+    Pry = ry * ry
+    Px1 = x1 * x1
+    Py1 = y1 * y1
+
+    # check that radii are large enough
+
+    radiiCheck = Px1 / Prx + Py1 / Pry if Prx != 0 and Pry != 0 else 0
+    if (radiiCheck > 1):
+        rx = sqrt(radiiCheck) * rx
+        ry = sqrt(radiiCheck) * ry
+        Prx = rx * rx
+        Pry = ry * ry
+
+    # Step 2 : Compute (cx1, cy1)
+    sign = -1 if largeArcFlag == sweepFlag else 1
+    sq = 0
+    if Prx * Py1 + Pry * Px1 > 0:
+        sq = (Prx * Pry - Prx * Py1 - Pry * Px1) / (Prx * Py1 + Pry * Px1)
+    sq = max(sq, 0)
+    coef = sign * sqrt(sq)
+    cx1 = coef * ((rx * y1) / ry)
+    cy1 = coef * -((ry * x1) / rx) if rx != 0 else 0
+
+    # Step 3 : Compute (cx, cy) from (cx1, cy1)
+    sx2 = (x0 + x) / 2.0
+    sy2 = (y0 + y) / 2.0
+    # print(x0, x)
+    cx = sx2 + (cosAngle * cx1 - sinAngle * cy1)
+    cy = sy2 + (sinAngle * cx1 + cosAngle * cy1)
+
+    # Step 4 : Compute the angleExtent (dangle)
+    ux = (x1 - cx1) / rx if rx != 0 else 0
+    uy = (y1 - cy1) / ry if ry != 0 else 0
+    vx = (-x1 - cx1) / rx if rx != 0 else 0
+    vy = (-y1 - cy1) / ry if ry != 0 else 0
+
+    # Compute the angle extent
+    n = sqrt((ux * ux + uy * uy) * (vx * vx + vy * vy))
+    p = ux * vx + uy * vy
+    sign = -1 if (ux * vy - uy * vx) < 0 else 1
+    if n != 0:
+        angleExtent = to_degrees(
+            sign * acos(p / n)) if abs(p/n) < 1 else 360+359
+    else:
+        angleExtent = 360+359
+    if (not(sweepFlag) and angleExtent > 0):
+        angleExtent -= 360
+    elif (sweepFlag and angleExtent < 0):
+        angleExtent += 360
+
+    angleExtent_sign = 1 if angleExtent < 0 else -1
+    angleExtent = (abs(angleExtent) % 360) * angleExtent_sign
+    # angleExtent %= 360
+
+    return cx, cy, angleExtent
+
+# ---------------------------------------
+
+def fp_to_ki(dim: float):
+    if dim not in ['', None] and isnan(float(dim)) == False:
+        return round(float(dim) * 10 * 0.0254, 2)
+
+# ---------------------------------------
+
+def drill_to_ki(hole_radius: float, hole_length: float, pad_height: float, pad_width: float):
+    if hole_radius > 0 and hole_length != '' and hole_length != None and hole_length != 0:
+
+        max_distance_hole = max(hole_radius*2, hole_length)
+        pos_0 = pad_height - max_distance_hole
+        pos_90 = pad_width - max_distance_hole
+        max_distance = max(pos_0, pos_90)
+
+        if max_distance == pos_0:
+            return "(drill oval {} {})".format(hole_radius*2, hole_length)
+        else:
+            return "(drill oval {} {})".format(hole_length, hole_radius*2)
+    if hole_radius > 0:
+        return "(drill {})".format(2 * hole_radius)
+    return ''
+
+# ---------------------------------------
+
+
+def angle_to_ki(rotation: float):
+    if isnan(rotation) == False:
+        return -(360 - rotation) if rotation > 180 else rotation
+    return ''
+
+# ---------------------------------------
+
+
+def rotate(x: float, y: float, degrees: float):
+    radians = (degrees/180)*2*pi
+    new_x = x*cos(radians) - y*sin(radians)
+    new_y = x*sin(radians) + y*cos(radians)
+    return new_x, new_y
+
+# ---------------------------------------
+
+
+class exporter_footprint_kicad():
+
+    def __init__(self, footprint: ee_footprint):
+        self.input = footprint
+        if type(self.input) is not ee_footprint:
+            print("Conversion non supportée")
+        else:
+            self.generate_kicad_footprint()
+
+    def generate_kicad_footprint(self):
+
+        # Convert dimension from easyeda to kicad
+        self.input.bbox.convert_to_mm()
+        for fields in (self.input.pads, self.input.tracks, self.input.holes, self.input.circles, self.input.rectangles, self.input.texts):
+            for field in fields:
+                field.convert_to_mm()
+
+        ki_info = ki_footprint_info(
+            name=self.input.info.name,
+            fp_type=self.input.info.fp_type
+        )
+
+        self.output = ki_footprint(
+            info=ki_info,
+        )
+
+        # For pads
+        for ee_pad in self.input.pads:
+            ki_pad = ki_footprint_pad(
+                type='thru_hole' if ee_pad.hole_radius > 0 else 'smd',
+                shape=KI_PAD_SHAPE[ee_pad.shape] if ee_pad.shape in KI_PAD_SHAPE else 'custom',
+                pos_x=ee_pad.center_x - self.input.bbox.x,
+                pos_y=ee_pad.center_y - self.input.bbox.y,
+                width=max(ee_pad.width, 0.01),
+                height=max(ee_pad.height, 0.01),
+                layers=KI_PAD_LAYER[ee_pad.layer_id] if ee_pad.layer_id in KI_PAD_LAYER else '',
+                number=ee_pad.number,
+                drill=0.0,
+                orientation=angle_to_ki(ee_pad.rotation),
+                polygon="",
+            )
+            ki_pad.drill = drill_to_ki(ee_pad.hole_radius, ee_pad.hole_length, ki_pad.height, ki_pad.width)
+            if '(' in ki_pad.number and ')' in ki_pad.number:
+                ki_pad.number = ki_pad.number.split('(')[1].split(')')[0]
+
+            # For custom polygon
+            is_custom_shape = (ki_pad.shape == 'custom')
+            point_list = [fp_to_ki(point) for point in ee_pad.points.split(' ')]
+            if is_custom_shape:
+                if len(point_list) <= 0:
+                    print("PAD ${id} is a polygon, but has no points defined".format(ee_pad.id))
+                else:
+                    # Replace pad width & height since kicad doesn't care
+                    ki_pad.width = 1.0
+                    ki_pad.height = 1.0
+
+                                # Generate polygon
+                    path = "".join('(xy {} {})'.format(
+                        round(point_list[i] - self.input.bbox.x, 2),
+                        round(point_list[i+1] - self.input.bbox.y, 2)
+                    ) for i in range(0, len(point_list), 2))
+                    ki_pad.polygon = f"\n\t\t(primitives \n\t\t\t(gr_poly \n\t\t\t\t(pts {path}\n\t\t\t\t) \n\t\t\t\t(width 0.1) \n\t\t\t)\n\t\t)\n\t"
+
+            self.output.pads.append(ki_pad)
+
+        # For tracks
+        for ee_track in self.input.tracks:
+            ki_track = ki_footprint_track(
+                layers=KI_PAD_LAYER[ee_track.layer_id] if ee_track.layer_id in KI_PAD_LAYER else 'F.Fab',
+                stroke_width=max(ee_track.stroke_width, 0.01)
+            )
+
+            # Generate line
+            point_list = [fp_to_ki(point) for point in ee_track.points.split(' ')]
+            for i in range(0, len(point_list)-2, 2):
+                ki_track.points_start_x.append(
+                    round(point_list[i] - self.input.bbox.x, 2))
+                ki_track.points_start_y.append(
+                    round(point_list[i+1] - self.input.bbox.y, 2))
+                ki_track.points_end_x.append(
+                    round(point_list[i+2] - self.input.bbox.x, 2))
+                ki_track.points_end_y.append(
+                    round(point_list[i+3] - self.input.bbox.y, 2))
+
+            self.output.tracks.append(ki_track)
+
+        # For holes
+        for ee_hole in self.input.holes:
+            ki_hole = ki_footprint_hole(
+                pos_x=ee_hole.center_x - self.input.bbox.x,
+                pos_y=ee_hole.center_y - self.input.bbox.y,
+                size=ee_hole.radius*2
+            )
+
+            self.output.holes.append(ki_hole)
+
+        # For circles
+        for ee_circle in self.input.circles:
+            ki_circle = ki_footprint_circle(
+                cx=ee_circle.cx - self.input.bbox.x,
+                cy=ee_circle.cy - self.input.bbox.y,
+                end_x=0.0,
+                end_y=0,
+                layers=KI_LAYERS[ee_circle.layer_id] if ee_circle.layer_id in KI_LAYERS else 'F.Fab',
+                stroke_width=max(ee_circle.stroke_width, 0.01)
+            )
+            ki_circle.end_x = ki_circle.cx + ee_circle.radius
+            self.output.circles.append(ki_circle)
+
+        # For rectangles
+        for ee_rectangle in self.input.rectangles:
+            ki_rectangle = ki_footprint_rectangle(
+                layers=KI_PAD_LAYER[ee_rectangle.layer_id] if ee_rectangle.layer_id in KI_PAD_LAYER else 'F.Fab',
+                stroke_width=max(ee_rectangle.stroke_width, 0.01)
+            )
+
+            start_x = ee_rectangle.x - self.input.bbox.x
+            start_y = ee_rectangle.y - self.input.bbox.y
+            width   = ee_rectangle.width
+            height  = ee_rectangle.height
+
+            ki_rectangle.points_start_x = [start_x, start_x + width, start_x + width, start_x]
+            ki_rectangle.points_start_y = [start_y, start_y, start_y + height, start_y]
+            ki_rectangle.points_end_x   = [start_x + width, start_x + width, start_x, start_x] 
+            ki_rectangle.points_end_y   = [start_y, start_y + height, start_y + height, start_y]
+
+            self.output.rectangles.append(ki_rectangle)
+
+        # For arcs
+        for ee_arc in self.input.arcs:
+            arc_path = ee_arc.path.replace(',', ' ').replace('M ', 'M').replace('A ', 'A')
+
+            start_x, start_y = arc_path.split('A')[0][1:].split(' ', 1)
+            start_x = fp_to_ki(start_x) - self.input.bbox.x
+            start_y = fp_to_ki(start_y) - self.input.bbox.y
+
+            arc_parameters = arc_path.split('A')[1].replace('  ', ' ')
+            svg_rx, svg_ry, xAxisRotation, largeArc, sweep, end_x, end_y = arc_parameters.split(' ', 6)
+            rx, ry = rotate(fp_to_ki(svg_rx), fp_to_ki(svg_ry), 0)
+
+            end_x = fp_to_ki(end_x) - self.input.bbox.x
+            end_y = fp_to_ki(end_y) - self.input.bbox.y
+            if ry != 0:
+                cx, cy, extent = compute_arc(start_x, start_y, rx, ry, float(
+                    xAxisRotation), largeArc == '1', sweep == '1', end_x, end_y)
+            else:
+                cx = 0.0
+                cy = 0.0
+                extent = 0.0
+
+            ki_arc = ki_footprint_arc(
+                start_x=cx,
+                start_y=cy,
+                end_x=end_x,
+                end_y=end_y,
+                angle=extent,
+                layers=KI_LAYERS[ee_arc.layer_id] if ee_arc.layer_id in KI_LAYERS else 'F.Fab',
+                stroke_width=max(fp_to_ki(ee_arc.stroke_width), 0.01)
+            )
+            self.output.arcs.append(ki_arc)
+
+        # For texts
+        for ee_text in self.input.texts:
+            ki_text = ki_footprint_text(
+                pos_x=ee_text.center_x - self.input.bbox.x,
+                pos_y=ee_text.center_y - self.input.bbox.y,
+                orientation=angle_to_ki(ee_text.rotation),
+                text=ee_text.text,
+                layers=KI_LAYERS[ee_text.layer_id] if ee_text.layer_id in KI_LAYERS else 'F.Fab',
+                font_size=max(ee_text.font_size, 1),
+                thickness=max(ee_text.stroke_width, 0.01),
+                display=' hide' if ee_text.is_displayed == False else '',
+                mirror=""
+            )
+            ki_text.layers = ki_text.layers.replace(
+                '.SilkS', '.Fab') if ee_text.type == 'N' else ki_text.layers
+            ki_text.mirror = ' mirror' if ki_text.layers[0] == 'B' else ''
+            self.output.texts.append(ki_text)
+
+    def get_ki_footprint(self):
+        return self.output
+
+    def export_footprint(self):
+        ki = self.output
+        ki_lib = ""
+
+        ki_lib += KI_MODULE_INFO.format(
+            package_lib="easyeda2kicad",
+            package_name=ki.info.name,
+            edit="5DC5F6A4"
+        )
+
+        if ki.info.fp_type and ki.info.fp_type =='smd':
+            ki_lib += KI_FP_TYPE.format(
+                component_type=ki.info.fp_type
+            )
+
+        # Get y_min and y_max to put component info
+        y_low = min(pad.pos_y for pad in ki.pads)
+        y_high = max(pad.pos_y for pad in ki.pads)
+
+        ki_lib += KI_REFERENCE.format(
+            pos_x="0", pos_y=y_low-4
+        )
+
+        ki_lib += KI_PACKAGE_VALUE.format(
+            package_name=ki.info.name, pos_x="0", pos_y=y_high+4
+        )
+        ki_lib += KI_FAB_REF
+
+        # ---------------------------------------
+
+        for track in ki.tracks + ki.rectangles:
+            for i in range(len(track.points_start_x)):
+                ki_lib += KI_LINE.format(
+                    start_x=track.points_start_x[i],
+                    start_y=track.points_start_y[i],
+                    end_x=track.points_end_x[i],
+                    end_y=track.points_end_y[i],
+                    layers=track.layers,
+                    stroke_width=track.stroke_width
+                )
+
+        for pad in ki.pads:
+            ki_lib += KI_PAD.format(**vars(pad))
+
+        for hole in ki.holes:
+            ki_lib += KI_HOLE.format(**vars(hole))
+
+        for circle in ki.circles:
+            ki_lib += KI_CIRCLE.format(**vars(circle))
+
+        for arc in ki.arcs:
+            ki_lib += KI_ARC.format(**vars(arc))
+
+        for text in ki.texts:
+            ki_lib += KI_TEXT.format(**vars(text))
+
+        ki_lib += KI_END_FILE
+
+        return ki_lib, ki.info.name
---
+++ core/kicad/parameters_kicad.py
@@ -0,0 +1,424 @@
+from dataclasses import dataclass, field, fields
+from enum import Enum
+from typing import List
+
+
+# ---------------- CONFIG ----------------
+
+# Origin point.
+KI_XO = 0
+KI_YO = 0
+
+# Settings for box drawn around pins in a unit.
+KI_DEFAULT_BOX_LINE_WIDTH = 0
+
+# Mapping from understandable schematic symbol box fill-type name 
+# to the fill-type indicator used in the KiCad part library.
+KI_BOX_FILLS = {"no_fill": "N", "fg_fill": "F", "bg_fill": "f"}
+KI_DEFAULT_BOX_FILL = "bg_fill"
+
+# Part reference.
+KI_REF_SIZE = 60  # Font size.
+KI_REF_Y_OFFSET = 200
+
+# Part number.
+KI_PART_NUM_SIZE = 60  # Font size.
+KI_PART_NUM_Y_OFFSET = 200
+
+# Part footprint
+KI_PART_FOOTPRINT_SIZE = 60  # Font size.
+KI_PART_FOOTPRINT_Y_OFFSET = KI_PART_NUM_Y_OFFSET + 100
+
+# Part manufacturer number.
+KI_PART_MPN_SIZE = 60  # Font size.
+KI_PART_MPN_Y_OFFSET = KI_PART_FOOTPRINT_Y_OFFSET + 100
+
+# Part datasheet.
+KI_PART_DATASHEET_SIZE = 60  # Font size.
+KI_PART_DATASHEET_Y_OFFSET = KI_PART_MPN_Y_OFFSET + 100 
+
+# Part description.
+KI_PART_DESC_SIZE = 60  # Font size.
+KI_PART_DESC_Y_OFFSET = KI_PART_DATASHEET_Y_OFFSET + 100
+
+
+KI_ROTATION = {"left": 0, "right": 180, "bottom": 90, "top": -90}
+
+KI_PIN_LENGTH = 100
+KI_PIN_SPACING = 100
+KI_PIN_NUM_SIZE = 50  # Font size for pin numbers.
+KI_PIN_NAME_SIZE = 50  # Font size for pin names.
+KI_PIN_NAME_OFFSET = 40  # Separation between pin and pin name.
+KI_PIN_ORIENTATION = "left"
+KI_PIN_STYLE = "line"
+KI_SHOW_PIN_NUMBER = True  # Show pin numbers when True.
+KI_SHOW_PIN_NAME = True  # Show pin names when True.
+KI_SINGLE_PIN_SUFFIX = ""
+KI_MULTI_PIN_SUFFIX = "*"
+KI_PIN_SPACER_PREFIX = "*"
+
+# --------------
+
+KI_LIB_HEADER = "EESchema-LIBRARY Version 2.3\n#encoding utf-8\n"
+KI_START_DEF = "DEF {name} {ref} 0 {pin_name_offset} {show_pin_number} {show_pin_name} {num_units} L N\n"
+KI_REF_FIELD = 'F0 "{ref_prefix}" {x} {y} {font_size} H V {text_justification} CNN\n'
+KI_PARTNUM_FIELD = 'F1 "{num}" {x} {y} {font_size} H V {text_justification} CNN\n'
+KI_FOOTPRINT_FIELD = 'F2 "{footprint}" {x} {y} {font_size} H I {text_justification} CNN\n'
+KI_DATASHEET_FIELD = 'F3 "{datasheet}" {x} {y} {font_size} H I {text_justification} CNN\n'
+KI_MPN_FIELD = 'F4 "{manufacturer}" 0 0 0 H I C CNN "Manufacturer"\n'
+KI_DESC_FIELD = 'F5 "{desc}" 0 0 0 H I C CNN "desc"\n'
+KI_LCSC_FIELD = 'F6 "{id}" 0 0 0 H I C CNN "LCSC Part"\n'
+KI_JLCPCB_FIELD = 'F7 "{type}" 0 0 0 H I C CNN "JLC Part"\n'
+
+KI_START_DRAW = "DRAW\n"
+
+KI_BOX = "S {x0} {y0} {x1} {y1} {unit_num} 1 {line_width} {fill}\n"
+KI_PIN = "X {name} {num} {x} {y} {length} {orientation} {num_sz} {name_sz} {unit_num} 1 {pin_type} {pin_style}\n"
+KI_POLYLINE = "P {points_number} {unit_num} 1 {line_width} {coordinate} {fill}\n"
+
+KI_END_DRAW = "ENDDRAW\n"
+KI_END_DEF = "ENDDEF\n"
+
+# --------------
+
+KI_PIN_TYPES = {
+    "input": "I",
+    "inp": "I",
+    "in": "I",
+    "clk": "I",
+    "output": "O",
+    "outp": "O",
+    "out": "O",
+    "bidirectional": "B",
+    "bidir": "B",
+    "bi": "B",
+    "inout": "B",
+    "io": "B",
+    "iop": "B",
+    "tristate": "T",
+    "tri": "T",
+    "passive": "P",
+    "pass": "P",
+    "unspecified": "U",
+    "un": "U",
+    "": "U",
+    "analog": "U",
+    "power_in": "W",
+    "pwr_in": "W",
+    "pwrin": "W",
+    "power": "W",
+    "pwr": "W",
+    "ground": "W",
+    "gnd": "W",
+    "power_out": "w",
+    "pwr_out": "w",
+    "pwrout": "w",
+    "pwr_o": "w",
+    "open_collector": "C",
+    "opencollector": "C",
+    "open_coll": "C",
+    "opencoll": "C",
+    "oc": "C",
+    "open_emitter": "E",
+    "openemitter": "E",
+    "open_emit": "E",
+    "openemit": "E",
+    "oe": "E",
+    "no_connect": "N",
+    "noconnect": "N",
+    "no_conn": "N",
+    "noconn": "N",
+    "nc": "N",
+}
+
+KI_PIN_STYLES = {
+    "line": "",
+    "": "",
+    "inverted": "I",
+    "inv": "I",
+    "~": "I",
+    "#": "I",
+    "clock": "C",
+    "clk": "C",
+    "rising_clk": "C",
+    "inverted_clock": "IC",
+    "inv_clk": "IC",
+    "clk_b": "IC",
+    "clk_n": "IC",
+    "~clk": "IC",
+    "#clk": "IC",
+    "input_low": "L",
+    "inp_low": "L",
+    "in_lw": "L",
+    "in_b": "L",
+    "in_n": "L",
+    "~in": "L",
+    "#in": "L",
+    "clock_low": "CL",
+    "clk_low": "CL",
+    "clk_lw": "CL",
+    "output_low": "V",
+    "outp_low": "V",
+    "out_lw": "V",
+    "out_b": "V",
+    "out_n": "V",
+    "~out": "V",
+    "#out": "V",
+    "falling_edge_clock": "F",
+    "falling_clk": "F",
+    "fall_clk": "F",
+    "non_logic": "X",
+    "nl": "X",
+    "analog": "X",
+}
+
+KI_PIN_ORIENTATIONS = {
+    "": "R",
+    "left": "R",
+    "right": "L",
+    "bottom": "U",
+    "down": "U",
+    "top": "D",
+    "up": "D",
+}
+
+class kicad_pin_orientation(Enum):
+    right = 0
+    top = 90
+    left = 180
+    bottom = 270
+
+# ---------------------------- SYMBOL PART ----------------------------
+
+# ---------------- INFO HEADER ----------------
+@dataclass
+class ki_symbol_info():
+    name:str
+    prefix:str
+    package:str
+    manufacturer:str
+    datasheet:str
+    lcsc_id:str
+    jlc_id:str
+
+# ---------------- PIN ----------------
+@dataclass
+class ki_symbol_pin():
+    name:str
+    number:str
+    style:str
+    type:float
+    orientation:float
+    pos_x:int
+    pos_y:int
+
+# ---------------- RECTANGLE ----------------
+@dataclass
+class ki_symbol_rectangle():
+    pos_x0:int=0
+    pos_y0:int=0
+    pos_x1:int=0
+    pos_y1:int=0
+
+# ---------------- POLYLINE ----------------
+@dataclass
+class ki_symbol_polyline():
+    points:List[List[int]] = field(default_factory=List[List[int]])
+    points_number:int=0
+
+# ---------------- SYMBOL ----------------
+@dataclass
+class ki_symbol():
+    info:ki_symbol_info
+    pins:List[ki_symbol_pin] = field(default_factory=List[ki_symbol_pin])
+    rectangles:List[ki_symbol_rectangle] = field(default_factory=List[ki_symbol_rectangle])
+    polylines:List[ki_symbol_polyline] = field(default_factory=List[ki_symbol_polyline])
+
+# ---------------------------- FOOTPRINT PART ----------------------------
+
+KI_MODULE_INFO     = "(module {package_lib}:{package_name} (layer F.Cu) (tedit {edit})\n"
+KI_DESCRIPTION     = '\t(descr "{datasheet_link}, generated with footprint_dumper on {date}")\n'
+KI_TAGS_INFO       = '\t(tags "{tag}")\n'
+KI_FP_TYPE         = '\t(attr {component_type})\n' 
+KI_REFERENCE       = "\t(fp_text reference REF** (at {pos_x} {pos_y}) (layer F.SilkS)\n\t\t(effects (font (size 1 1) (thickness 0.15)))\n\t)\n"
+KI_PACKAGE_VALUE   = "\t(fp_text value {package_name} (at {pos_x} {pos_y}) (layer F.Fab)\n\t\t(effects (font (size 1 1) (thickness 0.15)))\n\t)\n"
+KI_FAB_REF         = "\t(fp_text user %R (at 0 0) (layer F.Fab)\n\t\t(effects (font (size 1 1) (thickness 0.15)))\n\t)\n"
+KI_END_FILE        = ")"
+
+KI_PAD      = "\t(pad {number} {type} {shape} (at {pos_x:.2f} {pos_y:.2f} {orientation:.2f}) (size {width:.2f} {height:.2f}) (layers {layers}){drill}{polygon})\n"
+KI_LINE     = "\t(fp_line (start {start_x:.2f} {start_y:.2f}) (end {end_x:.2f} {end_y:.2f}) (layer {layers}) (width {stroke_width:.2f}))\n"
+KI_HOLE     = "\t(pad \"\" thru_hole circle (at {pos_x:.2f} {pos_y:.2f}) (size {size:.2f} {size:.2f}) (drill {size:.2f}) (layers *.Cu *.Mask))\n"
+KI_CIRCLE   = "\t(fp_circle (center {cx:.2f} {cy:.2f}) (end {end_x:.2f} {end_y:.2f}) (layer {layers}) (width {stroke_width:.2f}))\n"
+KI_ARC      = "\t(fp_arc (start {start_x:.2f} {start_y:.2f}) (end {end_x:.2f} {end_y:.2f}) (angle {angle:.2f}) (layer {layers}) (width {stroke_width:.2f}))\n"
+KI_TEXT     = '\t(fp_text user {text} (at {pos_x:.2f} {pos_y:.2f} {orientation:.2f}) (layer {layers}){display}\n\t\t(effects (font (size {font_size:.2f} {font_size:.2f}) (thickness {thickness:.2f})) (justify left{mirror}))\n\t)\n'
+
+# ---------------------------------------
+
+KI_PAD_SHAPE = {"ELLIPSE": "circle", "RECT": "rect", "OVAL": "oval", "POLYGON": "custom"}
+KI_PAD_LAYER = { 1: "F.Cu F.Paste F.Mask"
+            , 2: "B.Cu B.Paste B.Mask"
+            , 3: "F.SilkS"
+            , 11: "*.Cu *.Paste *.Mask"
+            , 13: "F.Fab"
+            , 15: "Dwgs.User"
+            }
+
+KI_LAYERS = {  1: 'F.Cu',
+            2: 'B.Cu',
+            3: 'F.SilkS',
+            4: 'B.SilkS',
+            5: 'F.Paste',
+            6: 'B.Paste',
+            7: 'F.Mask',
+            8: 'B.Mask',
+            10: 'Edge.Cuts',
+            11: 'Edge.Cuts',
+            12: 'Cmts.User',
+            13: 'F.Fab',
+            14: 'B.Fab',
+            15: 'Dwgs.User',
+            101:'F.Fab'
+            }
+
+# ---------------------------------------
+
+# Round all float values contained in the dataclass
+def round_float_values(self):
+    for field in fields(self):
+        current_value = getattr(self, field.name)
+        if isinstance(current_value, float):
+            setattr(self, field.name, round(current_value, 2))
+
+
+# ---------------- PAD ----------------
+@dataclass
+class ki_footprint_pad():
+    type:str
+    shape:str
+    pos_x:float
+    pos_y:float
+    width:float
+    height:float
+    layers:str
+    number:str
+    drill:float
+    orientation:float
+    polygon:str
+
+    def __post_init__(self):
+        round_float_values(self)
+        
+
+# ---------------- TRACK ----------------
+@dataclass
+class ki_footprint_track():
+    points_start_x:List[float] = field(default_factory=list)
+    points_start_y:List[float] = field(default_factory=list)
+    points_end_x:List[float] = field(default_factory=list)
+    points_end_y:List[float] = field(default_factory=list)
+    stroke_width:float = 0
+    layers:str = ''
+
+# ---------------- HOLE ----------------
+@dataclass
+class ki_footprint_hole():
+    pos_x:float
+    pos_y:float
+    size:float
+
+    def __post_init__(self):
+        round_float_values(self)
+
+# ---------------- CIRCLE ----------------
+@dataclass
+class ki_footprint_circle():
+    cx:float
+    cy:float
+    end_x:float
+    end_y:float
+    layers:str
+    stroke_width:float
+
+    def __post_init__(self):
+        round_float_values(self)
+
+# ---------------- RECTANGLE ----------------
+@dataclass
+class ki_footprint_rectangle(ki_footprint_track):
+    ...
+    # points_start_x:List[float] = field(default_factory=list)
+    # points_start_y:List[float] = field(default_factory=list)
+    # points_end_x:List[float] = field(default_factory=list)
+    # points_end_y:List[float] = field(default_factory=list)
+    # stroke_width:float = 0
+    # layers:str = ''
+
+# ---------------- ARC ----------------
+@dataclass
+class ki_footprint_arc():
+    start_x:float
+    start_y:float
+    end_x:float
+    end_y:float
+    angle:float
+    layers:str
+    stroke_width:float
+
+    def __post_init__(self):
+        round_float_values(self)
+
+# ---------------- TEXT ----------------
+@dataclass
+class ki_footprint_text():
+    pos_x:float
+    pos_y:float
+    orientation:float
+    text:str
+    layers:str
+    font_size:float
+    thickness:float
+    display:str
+    mirror:str
+
+    def __post_init__(self):
+        round_float_values(self)
+
+# ---------------- VIA ----------------
+@dataclass
+class ki_footprint_via():
+    name:str=""
+    # TODO
+
+# ---------------- SOLID REGION ----------------
+@dataclass
+class ki_footprint_solid_region():
+    name:str=""
+    # TODO
+
+# ---------------- COPPER AREA ----------------
+@dataclass
+class ki_footprint_copper_area():
+    name:str=""
+    # TODO
+
+# ---------------- FOOTPRINT INFO ----------------
+@dataclass
+class ki_footprint_info():
+    name:str
+    fp_type:str
+
+
+# ---------------- FOOTPRINT OBJ ----------------
+@dataclass
+class ki_footprint():
+    info:ki_footprint_info
+    pads:List[ki_footprint_pad] = field(default_factory=list)
+    tracks:List[ki_footprint_track] = field(default_factory=list)
+    vias:List[ki_footprint_via] = field(default_factory=list)
+    holes:List[ki_footprint_hole] = field(default_factory=list)
+    circles:List[ki_footprint_circle] = field(default_factory=list)
+    arcs:List[ki_footprint_arc] = field(default_factory=list)
+    rectangles:List[ki_footprint_rectangle] = field(default_factory=list)
+    texts:List[ki_footprint_text] = field(default_factory=list)
+    solid_regions:List[ki_footprint_solid_region] = field(default_factory=list)
+    copper_areas:List[ki_footprint_copper_area] = field(default_factory=list)
