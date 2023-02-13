29b5e0fbf312d80f287fb07187c78250013d454c
uPesy-computer <alexis@upesy.com>
2023-02-13 13:17:21 +0100

    Bump version: 0.6.2 → 0.6.3

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# easyeda2kicad v0.6.2
+# easyeda2kicad v0.6.3
 
 _________________
 [![PyPI version](https://badge.fury.io/py/easyeda2kicad.svg)](https://badge.fury.io/py/easyeda2kicad)
--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     ),
     long_description=long_description,
     long_description_content_type="text/markdown",
-    version="0.6.2",
+    version="0.6.3",
     author="uPesy",
     author_email="contact@upesy.com",
     url="https://github.com/uPesy/easyeda2kicad.py",
