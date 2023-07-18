e666c907b27e6616edd65472c8dd3584edbb413c
Matt Wildoer <mattwildoer@Matts-MacBook-Air.local>
2023-07-18 16:55:28 -0700

    Upgrade to Pydantic V2
    Pydantic V2 has breaking API changes, but provides a tool to automatically refactor code.
    This commit bumps the Pydantic version and upgrades the API calls using their tool.
    Migration guide: https://docs.pydantic.dev/2.0/migration/

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 with open("README.md") as fh:
     long_description = fh.read()
 
-production_dependencies = ["pydantic>=1.5", "requests>2.0.0"]
+production_dependencies = ["pydantic>=2.0.0", "requests>2.0.0"]
 
 development_dependencies = [
     "pre-commit>=2.17.0",
