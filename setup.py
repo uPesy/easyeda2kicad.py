b0cfadf7e622267d29aeb911ecf9bf12c7da08e7
Steffen Wittemeier <steffen.wittemeier@ruhr-uni-bochum.de>
2025-08-25 21:08:13 +0200

    clear dependencies

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 with open("README.md") as fh:
     long_description = fh.read()
 
-production_dependencies = ["pydantic>=2.0.0", "requests>2.0.0"]
+production_dependencies = []
 
 development_dependencies = [
     "pre-commit>=2.17.0",
