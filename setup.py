6f8cf879a51b62ce4cba43f4b62165da9cec4a5d
Steffen Wittemeier <uni@steffenwittemeier.de>
2025-12-21 17:13:42 +0100

    fix off grid symbol and increase version to 0.9.0 fix #60

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     ),
     long_description=long_description,
     long_description_content_type="text/markdown",
-    version="0.8.0",
+    version="0.9.0",
     author="uPesy",
     author_email="contact@upesy.com",
     url="https://github.com/uPesy/easyeda2kicad.py",
